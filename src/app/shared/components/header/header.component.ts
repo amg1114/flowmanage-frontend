36ec888cb83d9225f84e12be18aa83b1930dca10
Alejandro Moreno <alejandromogil@gmail.com>
2024-11-23 21:49:07 -0500

    feat: Add header and horizontal brand components with navigation links

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HorizontalBrandComponent } from '../horizontal-brand/horizontal-brand.component';
+
+@Component({
+  selector: 'app-header',
+  imports: [HorizontalBrandComponent],
+  templateUrl: './header.component.html',
+  styles: ``
+})
+export class HeaderComponent {
+
+}
