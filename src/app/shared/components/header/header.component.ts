2ef1e06a8e315a7fcbc6a3c75451609e86b5b1d2
Alejandro Moreno <alejandromogil@gmail.com>
2024-11-23 22:16:09 -0500

    feat: Enhance header component with responsive navigation and menu items

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { HorizontalBrandComponent } from '../horizontal-brand/horizontal-brand.component';
+import { LucideAngularModule, Menu } from 'lucide-angular';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [HorizontalBrandComponent],
+  imports: [
+    HorizontalBrandComponent,
+    LucideAngularModule,
+    RouterLink,
+    RouterLinkActive,
+    CommonModule,
+  ],
   templateUrl: './header.component.html',
-  styles: ``
+  styles: ``,
 })
 export class HeaderComponent {
-
+  readonly MenuIcon = Menu;
+  readonly menuItems = [
+    {
+      name: 'Home',
+      route: '/',
+    },
+    {
+      name: 'About',
+      route: '/about',
+    },
+    {
+      name: 'Contact',
+      route: '/contact',
+    },
+  ];
 }
