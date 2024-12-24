962775e8f4174b36952b3485fdf2a05e7ac45790
Alejandro Moreno <alejandromogil@gmail.com>
2024-12-24 12:21:15 -0300

    feat: Implement registration page with styled inputs and Google sign-in option for enhanced user experience

--- src/app/core/brand/auth/register/register-page.component.ts
+++ src/app/core/brand/auth/register/register-page.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { StyledInputComponent } from '@shared/components/forms/styled-input.component';
+import { GoogleIconComponent } from '@shared/components/icons/google-icon.component';
+import { StyledButtonComponent } from '@shared/components/typography/styled-button.component';
+import { StyledLinkComponent } from '@shared/components/typography/styled-link.component';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [
+    RouterLink,
+    StyledButtonComponent,
+    StyledLinkComponent,
+    StyledInputComponent,
+    GoogleIconComponent,
+  ],
   templateUrl: './register-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class RegisterPageComponent {
-
-}
+export class RegisterPageComponent {}
