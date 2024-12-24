a149c26dca90926bbec393335bd834a4757793a1
Alejandro Moreno <alejandromogil@gmail.com>
2024-12-23 21:20:09 -0300

    feat: Add hero component with background image and integrate into brand layout

--- src/app/core/brand/home/home-page.component.ts
+++ src/app/core/brand/home/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { StyledLinkComponent } from '../../../shared/components/typography/styled-link.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
+  imports: [StyledLinkComponent, RouterLink],
 })
 export class HomePageComponent {}
