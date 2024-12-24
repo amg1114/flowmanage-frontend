33c10b593f8bc23524632d293b89d85a6659bb3f
Alejandro Moreno <alejandromogil@gmail.com>
2024-12-24 14:35:50 -0300

    feat: Refactor brand component layout and adjust typography for improved responsiveness and readability

--- src/app/core/brand/brand.component.ts
+++ src/app/core/brand/brand.component.ts
@@ -7,8 +7,10 @@ import { RouterOutlet } from '@angular/router';
   imports: [HeaderComponent, RouterOutlet],
   template: `
     <app-header />
-    <main class="flex md:h-[70vh]">
-      <article class="w-full self-center md:max-w-md">
+    <main class="flex md:h-[70vh] md:w-2/5">
+      <article
+        class="h-[60vh] w-full self-center overflow-hidden md:h-auto md:max-w-md"
+      >
         <router-outlet></router-outlet>
       </article>
       <!-- Hero Thumb -->
@@ -18,7 +20,9 @@ import { RouterOutlet } from '@angular/router';
         <span
           class="absolute inset-0 rounded-t-full bg-light-secondary from-dark-secondary to-light-secondary opacity-85 md:rounded-t-none md:rounded-bl-3xl md:bg-gradient-to-r"
         ></span>
-        <figure class="absolute bottom-0 left-0 z-10 md:-translate-x-[20%]">
+        <figure
+          class="absolute bottom-0 left-1/2 z-10 mx-auto w-full max-w-xs -translate-x-1/2 md:left-0 md:max-w-screen-md md:-translate-x-[20%]"
+        >
           <img src="/hero/hero-figure.png" alt="App Indicators" width="750px" />
         </figure>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     colors: {
-      "light-primary": "#3A7DA6",
-      "dark-primary": "#599BC5",
-      "light-secondary": "#84C7F0",
-      "dark-secondary": "#0F517B",
-      "light-text": "#343536",
-      "dark-text": "#EBF0F4",
-      "light-bg": "#F4F9FB",
-      "dark-bg": "#04090B",
-      "light-accent": "#7FBDB1",
-      "dark-accent": "#428074",
-      transparent: "transparent",
+      'light-primary': '#3A7DA6',
+      'dark-primary': '#599BC5',
+      'light-secondary': '#84C7F0',
+      'dark-secondary': '#0F517B',
+      'light-text': '#343536',
+      'dark-text': '#EBF0F4',
+      'light-bg': '#F4F9FB',
+      'dark-bg': '#04090B',
+      'light-accent': '#7FBDB1',
+      'dark-accent': '#428074',
+      transparent: 'transparent',
     },
     fontFamily: {
-      roboto: ["Roboto Condensed", "sans-serif"],
-      inter: ["Inter", "sans-serif"],
+      roboto: ['Roboto Condensed', 'sans-serif'],
+      inter: ['Inter', 'sans-serif'],
     },
     container: {
       center: true,
       padding: {
-        DEFAULT: "1.25rem",
-        lg: "0",
+        DEFAULT: '1.25rem',
+        xl: '0',
       },
       screens: {
-        sm: "100%",
-        md: "100%",
-        lg: "1024px",
-        xl: "1280px",
+        sm: '100%',
+        md: '100%',
+        lg: '1024px',
+        xxl: '1280px',
       },
     },
     extend: {
@@ -38,5 +38,5 @@ export default {
       },
     },
   },
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: ['prettier-plugin-tailwindcss'],
 };
