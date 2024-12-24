09a5fe793f31cd821bf98112c2c440f62fb91134
Alejandro Moreno <alejandromogil@gmail.com>
2024-12-24 14:07:39 -0300

    feat: Adjust heading sizes and improve layout for consistency across authentication and home pages

--- src/app/core/brand/brand.component.ts
+++ src/app/core/brand/brand.component.ts
@@ -7,17 +7,18 @@ import { RouterOutlet } from '@angular/router';
   imports: [HeaderComponent, RouterOutlet],
   template: `
     <app-header />
-    <main class="container inset-0 flex md:h-[70vh]">
-      <article class="w-full max-w-md self-center">
+    <main class="flex md:h-[70vh]">
+      <article class="w-full self-center md:max-w-md">
         <router-outlet></router-outlet>
       </article>
+      <!-- Hero Thumb -->
       <div
-        class="absolute right-0 top-0 hidden h-[75vh] w-1/2 rounded-bl-3xl bg-heroThumb bg-cover bg-center bg-no-repeat md:block"
+        class="absolute bottom-0 left-0 h-[30vh] w-full rounded-t-full bg-cover bg-center bg-no-repeat md:bottom-auto md:left-auto md:right-0 md:top-0 md:block md:h-[75vh] md:w-1/2 md:rounded-t-none md:rounded-bl-3xl md:bg-heroThumb"
       >
         <span
-          class="absolute inset-0 rounded-bl-3xl bg-light-primary/85"
+          class="absolute inset-0 rounded-t-full bg-light-secondary from-dark-secondary to-light-secondary opacity-85 md:rounded-t-none md:rounded-bl-3xl md:bg-gradient-to-r"
         ></span>
-        <figure class="absolute bottom-0 left-0 z-10 -translate-x-[20%]">
+        <figure class="absolute bottom-0 left-0 z-10 md:-translate-x-[20%]">
           <img src="/hero/hero-figure.png" alt="App Indicators" width="750px" />
         </figure>
       </div>
