58ee654c2103778b9067360eb82eea581eac66d6
Alejandro Moreno <alejandromogil@gmail.com>
2024-12-23 21:42:13 -0300

    feat: Refactor hero component and update brand layout for improved responsiveness

--- src/app/core/brand/brand.component.ts
+++ src/app/core/brand/brand.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { RouterOutlet } from '@angular/router';
-import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-brand',
-  imports: [HeaderComponent, RouterOutlet, HeroComponent],
+  imports: [HeaderComponent, RouterOutlet],
   template: `
     <app-header />
-    <main class="h-[75vh] container inset-0 flex self-stretch">
+    <main class="container inset-0 flex md:h-[70vh]">
       <article class="self-center max-w-md">
         <router-outlet></router-outlet>
       </article>
-      <app-hero class="absolute w-1/2 right-0 top-0" />
+      <div
+        class="absolute w-1/2 right-0 top-0 hidden md:block h-[75vh] bg-no-repeat bg-cover bg-center bg-heroThumb rounded-bl-3xl"
+      >
+        <span
+          class="absolute inset-0 bg-light-primary/85 rounded-bl-3xl"
+        ></span>
+        <figure class="absolute z-10 -translate-x-[20%] bottom-0 left-0">
+          <img src="/hero/hero-figure.png" alt="App Indicators" width="750px" />
+        </figure>
+      </div>
     </main>
   `,
 })
