13673df136789dee348ab4b8f746a35903df75a5
Alejandro Moreno <alejandromogil@gmail.com>
2025-01-02 18:09:25 -0300

    feat: Refactor workflows component into workflow-listing for improved structure and user experience

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,50 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoggedUser } from '@app/core/interfaces/user/user.interface';
+
+import { UsersService } from '@app/core/services/users.service';
+import { LucideAngularModule, Loader } from 'lucide-angular';
+import { HeaderComponent } from './partials/header/header.component';
+import { DashboardConfigService } from '@app/core/services/dashboard-config.service';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [CommonModule, LucideAngularModule, HeaderComponent, RouterOutlet],
+  template: `
+    <main class="container">
+      <span
+        class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-light-secondary dark:text-dark-secondary"
+        *ngIf="loading"
+      >
+        <i-lucide [img]="LoaderIcon" class="h-16 w-16 animate-spin"></i-lucide>
+      </span>
+      <div
+        class="relative flex h-screen flex-col gap-8"
+        *ngIf="!loading && loggedUser"
+      >
+        <dashboard-header [loggedUser]="loggedUser" />
+        <router-outlet></router-outlet>
+      </div>
+    </main>
+  `,
+  styles: ``,
+})
+export class DashboardComponent implements OnInit {
+  loggedUser: LoggedUser | null = null;
+
+  loading = true;
+
+  readonly LoaderIcon = Loader;
+
+  constructor(private userService: UsersService) {
+    this.userService.loggedUser.subscribe((user) => {
+      if (user) {
+        this.loggedUser = user;
+        this.loading = false;
+      }
+    });
+  }
+
+  ngOnInit(): void {}
+}
--- src/app/pages/dashboard/workflows/workflow-listing/workflow-listing.component.ts
+++ src/app/pages/dashboard/workflows/workflow-listing/workflow-listing.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 
-import { ActionCreateComponent } from '../components/buttons/action-create.component';
-import { ActionFilterComponent } from '../components/buttons/action-filter.component';
-import { ActionInfoComponent } from '../components/buttons/action-info.component';
+import { ActionCreateComponent } from '../../components/buttons/action-create.component';
+import { ActionFilterComponent } from '../../components/buttons/action-filter.component';
+import { ActionInfoComponent } from '../../components/buttons/action-info.component';
 import { LoggedUser } from '@app/core/interfaces/user/user.interface';
 import { UsersService } from '@app/core/services/users.service';
 import { LucideAngularModule, Loader } from 'lucide-angular';
-import { WorkflowCardComponent } from '../components/workflows/workflow-card.component';
+import { WorkflowCardComponent } from '../../components/workflows/workflow-card.component';
 
 @Component({
-  selector: 'dashboard-workflows',
+  selector: 'dashboard-workflow-listing',
   imports: [
     ActionCreateComponent,
     ActionFilterComponent,
@@ -17,10 +17,10 @@ import { WorkflowCardComponent } from '../components/workflows/workflow-card.com
     LucideAngularModule,
     WorkflowCardComponent,
   ],
-  templateUrl: './workflows.component.html',
+  templateUrl: './workflow-listing.component.html',
   styles: ``,
 })
-export class WorkflowsComponent {
+export class WorkflowListingComponent {
   readonly LoadIcon = Loader;
 
   loggedUser: LoggedUser | null = null;
