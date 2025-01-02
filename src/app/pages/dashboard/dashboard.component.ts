28fb3c88ee82d1320b41d9494a878d08b2a6a9ed
Alejandro Moreno <alejandromogil@gmail.com>
2025-01-02 14:23:01 -0300

    feat: Implement workflow selection guard and dashboard configuration management

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,28 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { LoggedUser } from '@app/core/interfaces/user/user.interface';
-import { AuthService } from '@app/core/services/auth.service';
+
 import { UsersService } from '@app/core/services/users.service';
 import { LucideAngularModule, Loader } from 'lucide-angular';
 import { HeaderComponent } from './partials/header/header.component';
+import { DashboardConfigService } from '@app/core/services/dashboard-config.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule, LucideAngularModule, HeaderComponent],
+  imports: [CommonModule, LucideAngularModule, HeaderComponent, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styles: ``,
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   loggedUser: LoggedUser | null = null;
+
   loading = true;
 
   readonly LoaderIcon = Loader;
 
   constructor(private userService: UsersService) {
     this.userService.loggedUser.subscribe((user) => {
-      this.loading = false;
-      this.loggedUser = user;
+      if (user) {
+        this.loggedUser = user;
+        this.loading = false;
+      }
     });
   }
+
+  ngOnInit(): void {}
 }
---
+++ src/app/pages/dashboard/workflows/workflows.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dashboard-workflows',
+  imports: [],
+  templateUrl: './workflows.component.html',
+  styles: ``,
+})
+export class WorkflowsComponent {}
