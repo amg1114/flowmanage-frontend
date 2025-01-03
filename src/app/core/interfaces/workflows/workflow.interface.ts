9784e228ceff91a683699d82ffed3654c9a60697
Alejandro Moreno <alejandromogil@gmail.com>
2025-01-03 16:36:00 -0300

    feat: Extend Workflow interface to include status management

--- src/app/core/interfaces/workflows/workflow.interface.ts
+++ src/app/core/interfaces/workflows/workflow.interface.ts
@@ -1,4 +1,5 @@
 import { ResourceColors } from '@app/core/utils/constants/resource-colors.constants';
+import { WorkflowStatusType } from '@app/core/utils/status';
 
 export interface Workflow {
   id: number;
@@ -8,4 +9,16 @@ export interface Workflow {
   slug: string;
   description: null | string;
   color: ResourceColors;
+  status: WorkflowStatus[];
+}
+
+export interface WorkflowStatus {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+  name: string;
+  description: string;
+  type: WorkflowStatusType;
+  isDefault: boolean;
+  order: number;
 }
