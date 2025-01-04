a0169b197549a4970f14040d6e2a79b78c863112
Alejandro Moreno <alejandromogil@gmail.com>
2025-01-04 13:31:54 -0300

    feat: Implement dynamic workflow status management with add and remove functionality

--- src/app/core/services/workflows/workflows.service.ts
+++ src/app/core/services/workflows/workflows.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { WorkflowStatus } from '@app/core/interfaces/workflows/workflow.interface';
+import { getRandomResourceColor } from '@app/core/utils/colors';
 import { statusPlaceHolder } from '@app/core/utils/forms/status/create-status';
 import { workflowPlaceHolder } from '@app/core/utils/forms/workflows/create-workflow';
 
@@ -9,20 +11,78 @@ import { workflowPlaceHolder } from '@app/core/utils/forms/workflows/create-work
 export class WorkflowsService {
   static WORKFLOW_DRAFT_KEY = 'workflow-draft';
 
-  newWorkflow: FormGroup = workflowPlaceHolder;
+  newWorkflow: FormGroup;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
+    this.newWorkflow = this.fb.group({
+      title: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(80),
+        ],
+      ],
+      description: ['', [Validators.minLength(10), Validators.maxLength(120)]],
+      color: ['', Validators.required],
+      status: this.fb.array([]),
+    });
+
+    this.addDefaultData();
+  }
+
+  get workflowStatuses(): FormArray {
+    return this.newWorkflow.get('status') as FormArray;
+  }
+
+  addDefaultData(): void {
     const draft = localStorage.getItem(WorkflowsService.WORKFLOW_DRAFT_KEY);
+
     if (draft) {
-      this.newWorkflow.patchValue(JSON.parse(draft));
+      const parsedDraft = JSON.parse(draft);
+      this.newWorkflow.patchValue(parsedDraft);
+
       return;
     }
 
-    this.newWorkflow.get('status')?.setValue(statusPlaceHolder);
+    this.newWorkflow.patchValue({ color: getRandomResourceColor() });
+    statusPlaceHolder.forEach((status) => this.addStatus(status));
+  }
+
+  addStatus(status: Partial<WorkflowStatus>): void {
+    this.workflowStatuses.push(
+      this.fb.group({
+        id: [status.id],
+        name: [
+          status.name,
+          [
+            Validators.required,
+            Validators.minLength(5),
+            Validators.maxLength(80),
+          ],
+        ],
+        description: [
+          status.description,
+          [Validators.minLength(10), Validators.maxLength(120)],
+        ],
+        type: [status.type, [Validators.required]],
+      }),
+    );
+
+    this.storeWorkflowDraft();
+  }
+
+  removeStatus(status: any): void {
+    let statuses = this.workflowStatuses.getRawValue();
+    statuses = statuses.filter((s: any) => s.name !== status.name);
+
+    this.workflowStatuses.clear();
+    statuses.forEach((s: any) => this.addStatus(s));
   }
 
   storeWorkflowDraft(): void {
-    this.newWorkflow.markAllAsTouched();
+    if (!this.newWorkflow.valid) return;
+
     localStorage.setItem(
       WorkflowsService.WORKFLOW_DRAFT_KEY,
       JSON.stringify(this.newWorkflow.getRawValue()),
--- src/app/core/utils/forms/workflows/create-workflow.ts
+++ src/app/core/utils/forms/workflows/create-workflow.ts
@@ -15,21 +15,16 @@ export const workflowPlaceHolder = new FormGroup({
   color: new FormControl(getRandomResourceColor(), [Validators.required]),
   status: new FormArray([
     new FormGroup({
-      name: new FormControl('Pending', [Validators.required]),
-      description: new FormControl('Pending Tasks', [Validators.required]),
-      type: new FormControl(WorkflowStatusType.INACTIVE, [Validators.required]),
-    }),
-    new FormGroup({
-      name: new FormControl('In Progress', [Validators.required]),
-      description: new FormControl('In progress tasks', [Validators.required]),
-      type: new FormControl(WorkflowStatusType.ACTIVE, [Validators.required]),
-    }),
-    new FormGroup({
-      name: new FormControl('Completed', [Validators.required]),
-      description: new FormControl('Completed Tasks', [Validators.required]),
-      type: new FormControl(WorkflowStatusType.COMPLETED, [
+      name: new FormControl('', [
         Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(80),
       ]),
+      description: new FormControl('', [
+        Validators.minLength(10),
+        Validators.maxLength(120),
+      ]),
+      type: new FormControl('', [Validators.required]),
     }),
   ]),
 });
